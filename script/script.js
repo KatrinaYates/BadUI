3f8735a718e74c41460cb7d3c04f6c2fd80c997c
Katrina <Krmoore014@gmail.com>
2021-11-30 16:39:43 -0600

    .

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//Adding the title of a card to the cart when you click its button
 $(document).ready(function() {
     $(".addCart").click (function() {
         $("#cartText").append("<br>" + $(this).prev().prev().text() + '.........' + $(this).prev().text());
